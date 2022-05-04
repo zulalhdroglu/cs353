9ddc4f3edd9ec99da8f88da0dafacbcd0eb6b646
Zülal Nur Hıdıroğlu <nur.hidiroglu@ug.bilkent.edu.tr>
2022-05-04 08:41:52 +0300

    This helps to connect to localhost

---
+++ ConnectToDB.php
@@ -0,0 +1,16 @@
+<?php
+define('SERVER', "localhost");
+define("USER","root");
+define("PASS","WOIs8XDo");
+define("NAME", "school_library");
+class ConnectToDB {
+    public static function getConnection() {
+        $conn = new mysqli(SERVER, USER,PASS, NAME);
+        // Check for connection error
+        if ($conn->connect_error) {
+            die('Connection failed: ' . $conn->connect_error);
+        }
+        return $conn;
+    }
+}
+?>
