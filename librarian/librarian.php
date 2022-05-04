63123a5e45a5d8a180d44e5dd8bdb4a60c583471
Zülal Nur Hıdıroğlu <nur.hidiroglu@ug.bilkent.edu.tr>
2022-05-04 16:32:37 +0300

    It creates navgation bar for librarian

---
+++ librarian/librarian.php
@@ -0,0 +1,15 @@
+
+<!doctype html>
+<html>
+    <head>
+    <script src="//code.jquery.com/jquery.min.js"></script>
+    </head>
+    <div id="nav-placeholder">
+    </div>
+
+    <script>
+$(function(){
+  $("#nav-placeholder").load("navbar.html");
+});
+</script>
+</html>
